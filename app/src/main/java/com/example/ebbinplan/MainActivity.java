7b80d08a97beaa915fb199533a347fb4172a39d4
BenjaminFF <990460889@qq.com>
2019-11-15 18:08:11 +0800

    添加所有计划显示

--- app/src/main/java/com/example/ebbinplan/MainActivity.java
+++ app/src/main/java/com/example/ebbinplan/MainActivity.java
@@ -1,49 +1,30 @@
 package com.example.ebbinplan;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.PopupMenu;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.andexert.library.RippleView;
 import com.example.ebbinplan.model.PlanItem;
 import com.example.ebbinplan.planitem_list.MyAdapter;
-import com.example.ebbinplan.util.DensityUtil;
-import com.example.ebbinplan.util.KeyboardUtil;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Callback;
-import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.litepal.LitePal;
-import org.w3c.dom.Text;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
@@ -51,12 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,10 +40,13 @@ public class MainActivity extends AppCompatActivity {
     private CreatePlanDialog createPlanDialog;
     private RecyclerView planItemsRecyclerView;
     private TextView itemEmptyTextView;
+    private TextView planTextView;
     private ImageView titleImage;
     private MyAdapter myAdapter;
     private List<PlanItem> planItems;
+    private RippleView mPlanRippleView;
     private long minOfTodayTimeStamp = LocalDateTime.now().with(LocalTime.MIN).toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
+    private boolean isTodayPlan = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,96 +62,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void initTitle() {
         titleImage = findViewById(R.id.title_image);
-        MainActivity.this.runOnUiThread(new Runnable() {
+        titleImage.setAdjustViewBounds(true);
+        int[] pics = new int[]{R.mipmap.header_bg1, R.mipmap.header_bg2,
+                R.mipmap.header_bg1, R.mipmap.header_bg4,
+                R.mipmap.header_bg5, R.mipmap.header_bg6, R.mipmap.header_bg7, R.mipmap.header_bg8};
+        int pic = pics[(int) (Math.random() * pics.length)];
+        Picasso.get().load(pic).into(titleImage, new Callback() {
             @Override
-            public void run() {
-                int[] pics=new int[]{R.drawable.title_bg1,R.drawable.title_bg2, R.drawable.title_bg3,
-                        R.drawable.title_bg4,R.drawable.title_bg5,R.drawable.title_bg6,R.drawable.title_bg7};
-                int pic=pics[(int)(Math.random()*pics.length)];
-                Picasso.get().load(pic).into(titleImage, new Callback() {
-                    @Override
-                    public void onSuccess() {
-                        initCreatePlanDialog();
-                        initPlanItemsRecyclerView();
-                        initFAB();
-                    }
+            public void onSuccess() {
+                initCreatePlanDialog();
+                initRipple();
+                initPlanItemsRecyclerView();
+                initFAB();
+            }
 
-                    @Override
-                    public void onError(Exception e) {
+            @Override
+            public void onError(Exception e) {
 
-                    }
-                });
             }
         });
-        /*OkHttpClient client = new OkHttpClient.Builder()
-                .connectTimeout(50, TimeUnit.MILLISECONDS)
-                .writeTimeout(50, TimeUnit.MILLISECONDS)
-                .readTimeout(50, TimeUnit.MILLISECONDS)
-                .build();
-        Request request = new Request.Builder().url("https://api.unsplash.com/users/benjaminff/collections?client_id=1e3f66fcc0a140b1c80fb939b15a012428015720f91793d97b3daf9d6c54ef77").build();
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
+    }
+
+    private void initRipple() {
+        mPlanRippleView = findViewById(R.id.mPlanRippleView);
+        planTextView = findViewById(R.id.planTextView);
+        mPlanRippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
             @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                Log.d("LoadPicInfo", "请求图片列表数据失败");
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Picasso.get().load(R.drawable.title_bg3).into(titleImage);
-                        initCreatePlanDialog();
-                        initPlanItemsRecyclerView();
-                        initFAB();
-                    }
-                });
+            public void onComplete(RippleView rippleView) {
+                isTodayPlan = !isTodayPlan;
+                String mText = isTodayPlan ? "今日计划" : "全部计划";
+                planTextView.setText(mText);
+                togglePlan();
             }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                try {
-                    String resultJson = response.body().string();//5.获得网络数据
-                    JSONArray jsonArray = new JSONArray(resultJson);
-                    //final String picture = jsonArray.getJSONObject(0).getJSONObject("preview_photos").getJSONObject("").getString("raw") + "&h=800&fit=max";
-                    JSONArray photos = jsonArray.getJSONObject(0).getJSONArray("preview_photos");
-                    int picHeight= DensityUtil.dip2px(MainActivity.this,260);
-                    final String picture_url=photos.getJSONObject((int)(photos.length()*Math.random())).getJSONObject("urls").getString("raw")+ "&h="+picHeight+"&fit=max";
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Picasso.get().load(picture_url).networkPolicy(NetworkPolicy.OFFLINE).into(titleImage, new com.squareup.picasso.Callback() {
-                                @Override
-                                public void onSuccess() {
-                                    Log.d("LoadPicInfo", "加载缓存图片成功");
-                                    initCreatePlanDialog();
-                                    initPlanItemsRecyclerView();
-                                    initFAB();
-                                }
-
-                                @Override
-                                public void onError(Exception e) {
-                                    Log.d("LoadPicInfo", "加载缓存图片失败");
-                                    Picasso.get().load(picture_url).into(titleImage, new com.squareup.picasso.Callback() {
-                                        @Override
-                                        public void onSuccess() {
-                                            Log.d("LoadPicInfo", "加载网络图片成功");
-                                            initCreatePlanDialog();
-                                            initPlanItemsRecyclerView();
-                                            initFAB();
-                                        }
-
-                                        @Override
-                                        public void onError(Exception e) {
-                                            Log.d("LoadPicInfo", "加载网络图片失败");
-                                            //加载本地图片
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
-                } catch (JSONException e) {
-                }
-            }
-        });*/
+        });
     }
 
     private void initFAB() {
@@ -196,16 +118,16 @@ public class MainActivity extends AppCompatActivity {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        if(planItems.size()==0){
+                        if (planItems.size() == 0) {
                             itemEmptyTextView.setVisibility(View.VISIBLE);
                         }
                     }
-                },200);
+                }, 200);
             }
         });
         planItemsRecyclerView.setAdapter(myAdapter);
-        itemEmptyTextView=findViewById(R.id.main_item_empty_text);
-        if(planItems.size()==0){
+        itemEmptyTextView = findViewById(R.id.main_item_empty_text);
+        if (planItems.size() == 0) {
             MainActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -228,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
                 planItems.add(planItem);
             }
         }
-        if(itemEmptyTextView.getVisibility()==View.VISIBLE){
+        if (itemEmptyTextView.getVisibility() == View.VISIBLE) {
             MainActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -236,6 +158,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void togglePlan() {
+        planItems.clear();
+        if (isTodayPlan) {
+            planItems.addAll(LitePal.where("timestamp like ?", minOfTodayTimeStamp + "").find(PlanItem.class));
+        } else {
+            List<PlanItem> planList = LitePal.findAll(PlanItem.class);
+            List<PlanItem> newPlanList = new ArrayList<>(planList.size());
+            for (int i = 0; i < planList.size(); i++) {
+                boolean hasItem = false;
+                for (PlanItem item : newPlanList) {
+                    if (item.getPlanId().equals(planList.get(i).getPlanId())) {
+                        hasItem = true;
+                        break;
+                    }
+                }
+                if (!hasItem) {
+                    newPlanList.add(planList.get(i));
+                }
+            }
+            planItems.addAll(newPlanList);
+        }
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                int isVisible = planItems.size() == 0 ? View.VISIBLE : View.INVISIBLE;
+                itemEmptyTextView.setVisibility(isVisible);
+                myAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private void initCreatePlanDialog() {
@@ -257,6 +211,8 @@ public class MainActivity extends AppCompatActivity {
 
                 createPlan(planName);
                 createPlanDialog.dismiss();
+                editText.setText("");
+                myAdapter.notifyItemInserted(planItems.size() - 1);
             }
         });
         createPlanDialog.setOnCancelClickListener(new CreatePlanDialog.OnCancelClickListener() {
@@ -271,23 +227,6 @@ public class MainActivity extends AppCompatActivity {
                 }, 100);
             }
         });
-        createPlanDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                myAdapter.notifyItemInserted(planItems.size()-1);
-            }
-        });
-        /*createPlanDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        KeyboardUtil.toggleKeyboard(MainActivity.this);
-                    }
-                },200);
-            }
-        });*/
     }
 
     private void switchLanguage(String language) {
